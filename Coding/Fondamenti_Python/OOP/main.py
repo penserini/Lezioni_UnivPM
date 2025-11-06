cb02464ee3c03923fc20e8acb931145433d0b92b
penserini <98543600+penserini@users.noreply.github.com>
2025-11-06 10:56:37 +0100

    Delete main.py

--- Coding/Fondamenti_Python/OOP/main.py
+++ Coding/Fondamenti_Python/OOP/main.py
@@ -1,10 +1,1 @@
-from persona import Persona
-from studente import Studente
 
-Persona1 = Persona("Luca", "Rossi", 40, "lettura")
-Studente1 = Studente("Giulia", "Bianchi", 22, "musica", "Ingegneria Informatica")
-Studente2 = Studente(ind_studio="Matematica")
-
-print(Persona1.get_pag_benvenuto())
-print(Studente1.get_pag_benvenuto())   # chiamerà la versione ridefinita (IF)
-print(Studente2.get_pag_benvenuto())   # chiamerà la versione ridefinita (ELSE)
